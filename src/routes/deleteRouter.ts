043a77bbfdca49d29ed2ca7a846a57cea6e95592
PeterTikCheung <petermtcheung@gmail.com>
2024-07-08 11:14:26 +0800

    remove husky

--- src/routes/deleteRouter.ts
+++ src/routes/deleteRouter.ts
@@ -4,8 +4,6 @@ import deleteController from '@src/controllers/deleteController';
 
 const router = express.Router();
 
-
-
 router
   .delete('/room/:roomId', deleteController.deleteRoomById)
   .delete('/message/:messageId', deleteController.deleteMessageById);
